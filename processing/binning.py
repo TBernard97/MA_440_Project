a4e29e7e6a83c69ce16cacc905a12574d5084cb7
TBernard97 <darby.bernard@gmail.com>
2018-12-02 15:57:32 -0500

    Added Processing Directory

--- processing/binning.py
+++ processing/binning.py
@@ -1,17 +1,22 @@
 import pandas as pd 
+import matplotlib.pyplot as plt 
 
 
-df = pd.read_csv('../Data/For-Profit-C1.csv')
+
+df = pd.read_csv('../Data/For_Profit_C2.csv')
 
 labels = ['low', 'medium', 'standard', 'high']
 expense_bins = [0, 3000000, 6000000, 90000000, 300582000]
 salaries_bins = [0, 2500000, 4000000, 90000000, 128679000]
+grants_bins = [0, 800000, 4000000, 7000000, 53263100]
+tuition_bin = [0, 3000000, 6000000, 90000000, 300582000]
+
 
 df['Expense_bin'] = pd.cut(df['total_expenses'],bins=expense_bins,labels=labels)
 df['Salaries_bin'] = pd.cut(df['total_salaries'],bins=salaries_bins,labels=labels)
+df['Grants_bin'] = pd.cut(df['pell_grants'], bins=grants_bins, labels=labels)
 
 
+#df.to_csv('../Data/For-Profit-C1_Binned.csv')
 
-
-print(df)
-
+print(df['tuition_bin'].value_counts())
---
+++ search-algo.py
@@ -0,0 +1,38 @@
+import pandas as  pd 
+import csv
+
+
+df = pd.read_csv('uni.csv')
+
+comparison_file = input('Enter comparison file => ')
+output_file = input('Enter output file => ')
+data = input('Enter university list => ')
+
+
+dflist = pd.read_csv('{}'.format(data))
+comparison_frame = pd.read_csv('{}'.format(comparison_file))
+
+unilist = []
+
+for index, college in dflist.iterrows(): 
+   college  =  college['College']
+   unilist.append(college)
+
+
+id_list = []
+
+for college in unilist:
+   for index, name in df.iterrows():
+     if name.str.contains(college).any():
+         id_list.append(name['id'])
+
+with open('../Data/{}'.format(output_file), mode='a+') as output:
+    comparison_frame.head(0).to_csv(output)
+    for id in id_list:
+        for index, row in comparison_frame.iterrows():
+           if row.str.contains(id).any():
+               row = pd.DataFrame(row).T
+               row.to_csv(output, header=False)
+                
+       
+             
