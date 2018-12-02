0e6262fb1e69aaa9711c789a0b4e272ac1fe8b6b
TBernard97 <darby.bernard@gmail.com>
2018-12-02 08:09:20 -0500

    Finished Search Algo with File Output. Starting Classifiers

--- Info_Retireval/search-algo.py
+++ Info_Retireval/search-algo.py
@@ -1,10 +1,15 @@
 import pandas as  pd 
+import csv
 
 df = pd.read_csv('../Data/uni.csv')
 
+comparison_file = input('Enter comparison file => ')
+output_file = input('Enter output file => ')
+data = input('Enter university list => ')
 
-list = input('Enter university list => ')
-dflist = pd.read_csv('../Data/{}'.format(list))
+
+dflist = pd.read_csv('../Data/{}'.format(data))
+comparison_frame = pd.read_csv('../Data/{}'.format(comparison_file))
 
 unilist = []
 
@@ -13,11 +18,20 @@ for index, college in dflist.iterrows():
    unilist.append(college)
 
 
-results = []
+id_list = []
 
 for college in unilist:
    for index, name in df.iterrows():
-      if college in name['name']:
-       results.append(name['id'])
+     if name.str.contains(college).any():
+         id_list.append(name['id'])
 
-print(results)
+with open('../Data/{}'.format(output_file), mode='a+') as output:
+    comparison_frame.head(0).to_csv(output)
+    for id in id_list:
+        for index, row in comparison_frame.iterrows():
+           if row.str.contains(id).any():
+               row = pd.DataFrame(row).T
+               row.to_csv(output, header=False)
+                
+       
+             
